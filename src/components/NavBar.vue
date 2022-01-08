e1ae50ba592cc828242e239bb7907c4e8ce260bc
Quy P. TRAN <tranphuquy19@gmail.com>
2022-01-08 23:11:42 +0700

    update to vue router 4

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,21 +1,23 @@
 <template>
   <v-app-bar dense app color="primary" dark>
     <v-app-bar-nav-icon @click="onToggleDrawer()"></v-app-bar-nav-icon>
-    <router-link
-      :to="{ name: 'Home' }"
-      tag="div"
-      class="d-flex align-center"
-      id="home-icon"
-    >
-      <v-img
-        alt="Vuetify Logo"
-        class="shrink mr-2"
-        contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-        transition="scale-transition"
-        width="40"
-      />
-      <v-toolbar-title>DoraCI</v-toolbar-title>
+    <router-link :to="{ name: 'Home' }" custom v-slot="{ href, navigate }">
+      <div
+        class="d-flex align-center"
+        id="home-icon"
+        :href="href"
+        @click="navigate"
+      >
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="33"
+        />
+        <v-toolbar-title>DoraCI</v-toolbar-title>
+      </div>
     </router-link>
 
     <v-spacer></v-spacer>
